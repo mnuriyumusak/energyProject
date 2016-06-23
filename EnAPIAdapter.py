f55309683f267d654be0477b41a582fbbc773404
mnuriyumusak <mnuriyumusak@gmail.com>
2016-06-23 16:46:43 +0300

    v1

---
+++ EnAPIAdapter.py
@@ -0,0 +1,137 @@
+import json
+import urllib2
+import time
+import yaml
+global TOKEN
+TOKEN = ""
+
+BASE_URL_GOOGLE = "http://maps.google.com/maps/api/geocode/json?address="
+TOKEN_BASE_URL = "https://thepulseapi.earthnetworks.com/oauth20/token?grant_type=client_credentials&client_id="
+TEN_DAY_BASE_URL = "https://thepulseapi.earthnetworks.com/data/forecasts/v1/daily?location="
+HOURLY_BASE_URL = "https://thepulseapi.earthnetworks.com/getHourly6DayForecast/data/forecasts/v1/hourly?location="
+DEFAULT_HOURLY_PARAMS = ["forecastDateUtcStr", "cloudCoverPercent", "dewPoint", "precipCode", "precipProbability",
+                         "relativeHumidity", "temperature", "thunderstormProbability", "windDirectionDegrees",
+                         "windSpeed", "snowRate"]
+DEFAULT_TEN_DAY_PARAMS = ["forecastDateUtcStr","cloudCoverPercent", "dewPoint", "precipCode", "precipProbability",
+                          "relativeHumidity", "temperature", "thunderstormProbability", "windDirectionDegrees",
+                          "windSpeed", "snowAmountMm"]
+
+
+def set_token(arg):
+    global TOKEN
+    TOKEN = arg
+
+
+def get_json_data(final_url):
+    """
+    General function that takes and returns json data from given url
+    """
+    my_final_url = final_url + str(TOKEN)
+    try:
+        response = urllib2.urlopen(my_final_url)
+    except urllib2.HTTPError:
+        return_token()
+        set_token(return_token())
+        my_final_url = final_url + str(TOKEN)
+        response = urllib2.urlopen(my_final_url)
+
+    json_data = json.load(response)
+    return json_data
+
+
+def get_ten_day_forecast(infos, return_format, params=None):
+    """
+    Takes Json data from given parameters and returns it
+    """
+    final_url = TEN_DAY_BASE_URL + str(infos[4]) + "," + str(infos[5]) + \
+        "&locationtype=latitudelongitude&units=metric&cultureinfo=en-en&verbose=true&access_token="
+
+    json_data = get_json_data(final_url)
+    if return_format == "JSON":
+        return json_data
+    elif return_format == "CSV":
+        if params is None:
+            params = DEFAULT_TEN_DAY_PARAMS
+        return convert_to_csv_ten_day(infos, json_data, params)
+
+
+def get_hourly_forecast(infos,return_format, params=None):
+    final_url = HOURLY_BASE_URL + str(infos[4]) + "," + str(infos[5]) + \
+        "&locationtype=latitudelongitude&units=metric&cultureinfo=en-en&verbose=true&access_token="
+
+    json_data = get_json_data(final_url)
+
+    if return_format == "JSON":
+        return json_data
+    elif return_format == "CSV":
+        if params is None:
+            params = DEFAULT_HOURLY_PARAMS
+        return convert_to_csv_hourly(infos, json_data, params)
+
+
+def return_token():
+    """
+    Because Earth Networks refreshes token after some point, this functions refreshes the token to avoid errors
+    """
+    yaml_file = open("secrets.yaml", 'r')
+    yaml_dict = yaml.load(yaml_file)
+    client_id = str(yaml_dict["client_id"])
+    client_secret = str(yaml_dict["client_secret"])
+    yaml_file.close()
+    final_url = TOKEN_BASE_URL + client_id + "&client_secret=" + client_secret
+    try:
+        response = urllib2.urlopen(final_url)
+    except:
+        final_url = TOKEN_BASE_URL + client_id + "&client_secret=" + client_secret
+        response = urllib2.urlopen(final_url)
+    json_data = json.load(response)
+    token = json_data["OAuth20"]["access_token"]["token"]
+    refresh_token = json_data["OAuth20"]["access_token"]["refresh_token"]
+    if token != refresh_token:
+        return return_token()
+    return  refresh_token
+
+
+def convert_to_csv_ten_day(infos, json_data, params):
+    """
+    Creates a csv data from given parameters
+    """
+    json_data = json_data["dailyForecastPeriods"]
+    csv_string = ""
+    """
+    for i in range(2):
+        if i == 1:
+            if (int(str(json_data[i][params[0]][8:10])) <= int(time.strftime("%d"))) == False:
+                break
+    """
+
+    for count in range(len(params)):
+        if count == 0:
+            csv_string += "en_" + str(json_data[0][params[count]]) + ";"
+        else:
+            csv_string += str(json_data[0][params[count]])[0:len(str(json_data[0][params[count]]))-1] + ";"
+    csv_string += "%s;%s;%s;%s;%s;%s" % (infos[0], infos[1], infos[2], infos[3], infos[4], infos[5])
+    csv_string += "\n"
+    return csv_string
+
+
+def convert_to_csv_hourly(infos, json_data, params):
+    json_data = json_data["hourlyForecastPeriod"]
+    csv_string = ""
+
+    for row in json_data:
+        enter = False
+        for p in range(len(params)):
+            if p==0:
+                if int(str(row[params[p]][8:10])) <= int(time.strftime("%d")):
+                    enter = True
+                    csv_string += str(row[params[p]])[0:len(str(json_data[0][params[p]]))-1] + ";"
+                else:
+                    break
+            elif enter:
+                csv_string += str(row[params[p]]) + ";"
+        if enter:
+            csv_string += "%s;%s;%s;%s;%s;%s" % (infos[0], infos[1], infos[2], infos[3], infos[4], infos[5])
+            csv_string += "\n"
+
+    return csv_string
---
+++ deneme.py
@@ -0,0 +1,59 @@
+import utm
+import csv
+import EnAPIAdapter
+import time
+import os
+
+_OUTPUT_DIR = "output"
+
+
+def convert_utm_to_lat_long():
+    f = open('res_list.csv', 'a+')
+    csv_data = csv.reader(f, delimiter=';')
+    new_csv_data = ""
+    for item in csv_data:
+        lat_lng = utm.to_latlon(float(item[2]), float(item[3]), 35, 'U')
+        item.append(lat_lng[0])
+        item.append(lat_lng[1])
+        for idx, i in enumerate(item):
+            new_csv_data += str(i)
+            if idx != len(item)-1:
+                new_csv_data += ";"
+        new_csv_data += "\n"
+    f.close()
+    target = open("res_list_with_lat_long.csv", 'a+')
+    target.write(new_csv_data)
+    target.close()
+
+
+def write_forecast_to_csv():
+    try:
+        f = open('res_list_with_lat_long.csv', 'r+')
+        csv_data = csv.reader(f, delimiter=';')
+
+        file_name_hourly = _OUTPUT_DIR + "/en_%s_hourly.csv" % time.strftime("%Y%m%d")
+        file_name_daily = _OUTPUT_DIR + "/en_%s_10_day.csv" % time.strftime("%Y%m%d")
+        if not os.path.exists(os.path.dirname(file_name_hourly)):
+            os.makedirs(os.path.dirname(file_name_hourly))
+        target_h = open(file_name_hourly, 'a+')
+        target_d = open(file_name_daily, 'a+')
+        for i in csv_data:
+            line_h = EnAPIAdapter.get_hourly_forecast(i,"CSV")
+            target_h.write(line_h)
+            line_d = EnAPIAdapter.get_ten_day_forecast(i,"CSV")
+            target_d.write(line_d)
+        target_h.close()
+        target_d.close()
+        return True
+    except:
+        return False
+
+
+def main():
+    t0 = time.clock()
+    result = write_forecast_to_csv()
+    if not result:
+        print "A problem occured ! Mission failed !"
+    print str(time.clock()-t0)
+if __name__ == '__main__':
+    main()
